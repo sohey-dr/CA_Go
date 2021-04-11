51bb95ed3532ab293989a70660e4aa8f1eaea87b
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-12 01:29:54 +0900

    init

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"flag"
-
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"github.com/sohey-dr/CA_Go/config"
-	"github.com/sohey-dr/CA_Go/database"
-	"github.com/sohey-dr/CA_Go/server"
-)
+import "fmt"
 
 func main() {
-
-	env := flag.String("e", "development", "")
-	flag.Parse()
-
-	config.Init(*env)
-	database.Init(false)
-	if err := server.Init(); err != nil {
-		panic(err)
-	}
+  fmt.Println("Hello, World!")
 }
